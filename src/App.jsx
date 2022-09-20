852c8e5c8821374d7e5feead9424ab0166d0d519
Gutthred <fabioappelgren@icloud.com>
2022-09-20 17:00:33 -0300

    footer removal

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from '../src/components/header/Header'
 import NavBar from '../src/components/navBar/NavBar'
 import About from '../src/components/about/About'
 import Projects from '../src/components/projects/Projects'
-import Footer from '../src/components/footer/Footer'
+
 
 const App = () => {
   return (
@@ -11,7 +11,6 @@ const App = () => {
       <NavBar/>
       <About/>
       <Projects/>
-      <Footer/>
     </>
   )
 }
