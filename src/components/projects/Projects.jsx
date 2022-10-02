b494bf95bbe336ff40fdf7323731790ccc581b3f
Fabio Silva <53185249+Gutthred@users.noreply.github.com>
2022-10-02 17:27:25 -0300

    Repo section removed

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -7,9 +7,7 @@ const Projects = () => {
   return (
     <section className="projects__container" id="projects">
       <h2>Projetos</h2>
-      <section className="project__container">
       <Repo/>
-      </section>
     </section>
   );
   
